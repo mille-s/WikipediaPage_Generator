fe528812bb09c2c42725a2a60ee5297ab76e50e5
andradeM17 <mark.andrade@adaptcentre.ie>
2026-05-19 16:32:59 +0100

    Moving the logging statment

--- code/queryDBpediaProps.py
+++ code/queryDBpediaProps.py
@@ -75,11 +75,11 @@ def get_triples_seen(results, subj_name, triple_source, list_properties, ignore_
         prop_name = property_uri.removeprefix(url_triples) # Changed from property_uri.rsplit('/', 1)[1] to remove_prefix for better readability
         obj_name = value
         if re.search('http://', value):
+          print(f"Checking {value}...")
           if triple_source == 'Ontology': # Changed obj_name to remove the ontology url prefix for better readability
             obj_name = value.removeprefix('http://dbpedia.org/resource/')
             obj_is_dbo = True # Changed obj_is_dbo to True if the object is a dbo entity, which means we can check its types and compare them to the expected ranges of the property
           else:
-            print(obj_name, "is a non-dbo value.")
             obj_name = value
           obj_name = value.rsplit('/', 1)[1]
         obj_name_final = ''
